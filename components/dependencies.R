5535e8e4869a3e53f95d9939ed8290e661a87e5e
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-09-09 13:37:48 -0400

    Set up renv and initial repo structure

---
+++ components/dependencies.R
@@ -0,0 +1,10 @@
+# Any packages that need to be added to renv that are not added automatically
+# should get added in this file.
+#
+# For example, if you needed to include dplyr in renv, but there's no script
+# or notebook that uses it yet you would put the following in this script
+# (but uncommented):
+#
+# library(dplyr)
+#
+
