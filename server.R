9ebbd2b0890158572e369609a8896ab78c500fb9
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-10-20 13:16:15 -0400

    Fix edf signal parsing

--- server.R
+++ server.R
@@ -7,28 +7,20 @@ shinyServer(function(input, output) {
   observeEvent(input$go, {
 
     # Read in and reference the EDF data from input$edf_file ----------
-    #edf_data <- reactive({
-    #  infile <- input$edf_file
-    #  if (is.null(infile)) {
-    #    # User has not uploaded a file yet
-    #    return(NULL)
-    #  }
-    #  # Or, read in
-    #  read_edf_file(infile$datapath)
-    #})
     edf_data <- reactive({
-      read_edf_file()
+      infile <- input$edf_file
+      if (is.null(infile)) {
+        # User has not uploaded a file yet
+        return(NULL)
+      }
+      # Or, read in
+      read_edf_file(infile$datapath)
     })
-
-    ### Reactive variables for conditionalPanel interaction in ui.R
-    #output$data_exists <- reactive(!is.null(edf_data()))
-    #outputOptions(output, "data_exists", suspendWhenHidden = FALSE)
 
     # Prep PSD data -------------
     psd_data <- reactive({
       prep_psd_data(edf_data())
     })
-
 
 
     # Make and export PSD plot ----------
@@ -43,7 +35,7 @@ shinyServer(function(input, output) {
         plot <- make_psd_plot(psd_data(),
                               input$psd_plot_channels,
                               input$psd_frequency_range)
-        ggplot2::ggsave(file, plot, width = 6, height = 4)
+        ggplot2::ggsave(file, plot, width = 8, height = 5)
       }
     )
     output$psd_plot_ui <- renderUI({
